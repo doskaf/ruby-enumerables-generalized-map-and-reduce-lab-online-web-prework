00547a34f5afdd1ad0a31ca558cb9975c631b56a
Dominique Skaf <doskaf@yahoo.com>
2019-08-23 20:18:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array << yield(source_array[i])
+  i += 1
+  end
+  return new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  
+end
